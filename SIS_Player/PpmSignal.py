0ecd322821978ef0ff30a7b6c544203f9f66a4df
jonpas <jonpas33@gmail.com>
2018-04-22 18:21:25 +0200

    Combine axis_to_signal and to_signal functions, Add default values

--- SIS_Player/PpmSignal.py
+++ SIS_Player/PpmSignal.py
@@ -1,19 +1,20 @@
+import numpy as np
+
 
 class PpmSignal:
     def __init__(self):
         self.data = []
-        pass
 
-    # Transforms 4 doubles representing axis input of controller to 8 doubles representing length of correct channel
-    def axis_to_signal(self, roll, pitch, yaw, throttle):
-        pass
+    # Generates signal represented by the 8 inputs (normalized between -1 and 1)
+    # In drone control, axes represent: roll, pitch, yaw, throttle, aux1, aux2, aux3, aux4
+    def axis_to_signal(self, ax1=0, ax2=0, ax3=0, ax4=0, ax5=0, ax6=0, ax7=0, ax8=0):
+        signal = np.array([ax1, ax2, ax3, ax4, ax5, ax6, ax7, ax8])
 
-    # Generates signal represented by the 8 inputs
-    def to_signal(self, c1, c2, c3, c4, c5, c6, c7, c8):
-        pass
+        # TODO Insert no data sections correctly
+
+        return signal
 
     # Appends generated signal to data with correct modulation
+    # Signal contains 8 numbers representing length of each PPM section
     def append_to_data(self, signal):
         pass
-
-
