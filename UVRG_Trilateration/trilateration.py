a1848e2892fe1818256bcd3e5f6a0f3827bb6ebd
jonpas <jonpas33@gmail.com>
2018-05-24 16:43:50 +0200

    Final Trilateration cleanup (pep8, comments, remove unused variable)

--- UVRG_Trilateration/trilateration.py
+++ UVRG_Trilateration/trilateration.py
@@ -2,9 +2,9 @@
 
 import sys
 import argparse
+import random
 import math
 import numpy as np
-import random
 
 
 def main():
@@ -28,7 +28,7 @@ def main():
 
     # Remove all unset args
     p = [x for x in p if x.any()]
-    r = [x for x in r if x != None]
+    r = [x for x in r if x is not None]
 
     if len(p) == 3:
         k1, k2 = three_point_trilateration(p, r)
@@ -37,32 +37,27 @@ def main():
         print([round(i, 2) for i in k2])
     elif len(p) == 4:
         k1 = four_point_trilateration(p, r)
+
         print([round(i, 2) for i in k1])
     else:
-        # For saving previously picked stations
-        previous_positions = []
+        # Pick random unique combinations of stations
+        rand_stations = []
+        stations = p.copy()  # Copy for shuffling
 
-        # Pick stations
-        for i in range(len(p)):
-            stations = p.copy()
-
-            # Randomize
+        while len(rand_stations) < len(p):
+            # Randomize and pick first 4
             random.shuffle(stations)
             four_stations = stations[0:4]
 
             # Always pick different set of stations
-            while already_used_stations(four_stations, previous_positions):
-                random.shuffle(stations)
-                four_stations = stations[0:4]
+            if not already_used_stations(four_stations, rand_stations):
+                rand_stations.append(four_stations)
 
-            # Save station
-            previous_positions.append(four_stations)
-
-        P = [0, 0, 0] # End result
+        P = [0, 0, 0]  # End result
         positions = []
 
         # Calculate all positions
-        for stations in previous_positions:
+        for stations in rand_stations:
             # Get distances for current stations
             r_of_stations = []
             for s in stations:
@@ -93,6 +88,7 @@ def get_index(points, point):
         if np.array_equal(p, point):
             return i
 
+
 def already_used_stations(current, previous):
     # If no previous stations
     if len(previous) == 0:
@@ -112,6 +108,7 @@ def already_used_stations(current, previous):
             return True
 
     return False
+
 
 def three_point_trilateration(p, r):
     p1, p2, p3 = p[0], p[1], p[2]
@@ -144,6 +141,7 @@ def three_point_trilateration(p, r):
 
     return k1, k2
 
+
 def four_point_trilateration(p, r):
     p1, p2, p3, p4 = p[0], p[1], p[2], p[3]
     r1, r2, r3, r4 = r[0], r[1], r[2], r[3]
@@ -157,23 +155,22 @@ def four_point_trilateration(p, r):
     v1xv2 = np.cross(v1, v2)
     xn = v1 / np.linalg.norm(v1)
 
+    # Exit of vectors are collinear
     if np.linalg.norm(v1xv2) == 0:
         print("V1 and V2 are collinear")
-        quit()
-
+        sys.exit(1)
 
     zn = v1xv2 / np.linalg.norm(v1xv2)
     yn = np.cross(xn, zn)
 
-    # Calculate a,b and c from p4 and others locations
-    a = np.dot((p4 - p1), xn)
-    b = np.dot((p4 - p1), yn)
-    c = np.dot((p4 - p1), zn)
-    p4 = np.array([a, b, c])
-
     d = np.dot(xn, v1)  # p1 = [0, 0, 0]
     i = np.dot(xn, v2)  # p2 = [d, 0, 0]
     j = np.dot(yn, v2)  # p3 = [i, j, 0]
+
+    # p4 = [a, b, c]
+    a = np.dot((p4 - p1), xn)
+    b = np.dot((p4 - p1), yn)
+    c = np.dot((p4 - p1), zn)
 
     # Calculate target's position
     x = (r1**2 - r2**2 + d**2) / (2 * d)
@@ -189,9 +186,11 @@ def four_point_trilateration(p, r):
 
     return k
 
+
 # Compares 2 numbers if equal, designed for floats to overcome precision errors
 def almost_equal(a, b):
     return np.abs(a - b) < 0.000001
+
 
 if __name__ == "__main__":
     sys.exit(main())
